e321a338019e8f696556f28fcddddca51299081e
talhaceliktas <avrol928@gmail.com>
2025-10-14 23:32:14 +0300

    login ekrani tamamlandi

--- app/_components/SignIn.tsx
+++ app/_components/SignIn.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { openSans } from "../_lib/fonts";
 import { FaLock, FaUser } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const SignIn = () => {
   return (
@@ -13,7 +14,7 @@ const SignIn = () => {
         <p className="text-primary-50 opacity-75">
           Hoşgeldiniz! Lütfen gerekli alanları doldurunuz.
         </p>
-        <div className="mt-10 mb-8 flex w-full flex-col gap-5">
+        <div className="mt-4 mb-8 flex w-full flex-col gap-5">
           <div className="relative w-full">
             <input
               type="email"
@@ -32,19 +33,26 @@ const SignIn = () => {
             <FaLock className="peer-focus:fill-primary-50 fill-primary-300 absolute top-1/2 right-2 -translate-y-1/2 text-xl duration-300" />
             <span className="bg-primary-50/85 absolute bottom-0 left-0 h-[2px] w-0 transition-all duration-300 peer-focus:w-full"></span>
           </div>
-        </div>
-        <div className="flex w-full justify-between">
-          <div className="flex gap-x-2">
-            <input type="checkbox" className="block" id="beniHatirla" />
-            <label htmlFor="beniHatirla">Beni Hatırla</label>
+          <div className="flex w-full flex-col items-center justify-between gap-y-2 sm:flex-row sm:gap-y-0">
+            <div className="flex gap-x-2">
+              <input type="checkbox" className="block" id="beniHatirla" />
+              <label htmlFor="beniHatirla">Beni Hatırla</label>
+            </div>
+            <Link href={""} className="block text-sm">
+              Şifremi Unuttum
+            </Link>
           </div>
-          <Link href={""} className="block">
-            Şifremi Unuttum
-          </Link>
         </div>
-        <div className="mt-8 w-full text-center">
-          <button className="bg-primary-500 hover:bg-primary-600 mb-4 w-full cursor-pointer rounded-full py-2 duration-300">
+
+        <div className="w-full text-center">
+          <button className="bg-primary-500 hover:bg-primary-600 mb-4 w-full cursor-pointer rounded-2xl py-2 font-bold duration-300">
             Giriş Yap
+          </button>
+          <button className="bg-primary-50 mb-4 flex w-full cursor-pointer items-center justify-center gap-x-2 rounded-2xl py-2 duration-300">
+            <FcGoogle className="text-2xl" />
+            <span className="text-primary-700 font-bold">
+              Google ile Giriş Yap
+            </span>
           </button>
           <button className="hover:text-secondary-1 cursor-pointer text-xs duration-300">
             Hemen Kayıt Ol
