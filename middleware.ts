69fad601dbf76586df8199ce98afc5a9cd774e9d
talhaceliktas <avrol928@gmail.com>
2025-10-26 19:54:52 +0300

    middleware dosyasi eklendi

---
+++ middleware.ts
@@ -0,0 +1,57 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  console.log("Middleware girildi");
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name, value, options) {
+          request.cookies.set({ name, value, ...options });
+          response = NextResponse.next({
+            request: { headers: request.headers },
+          });
+          response.cookies.set({ name, value, ...options });
+        },
+        remove(name, options) {
+          request.cookies.delete(name);
+          response = NextResponse.next({
+            request: { headers: request.headers },
+          });
+          response.cookies.set({ name, value: "", ...options });
+        },
+      },
+    },
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user && request.nextUrl.pathname.startsWith("/profil")) {
+    return NextResponse.redirect(new URL("/giris", request.url));
+  }
+
+  if (user && request.nextUrl.pathname === "/giris") {
+    // Ana sayfaya yönlendir
+    return NextResponse.redirect(new URL("/profil", request.url));
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: ["/profil", "/giris"],
+};
